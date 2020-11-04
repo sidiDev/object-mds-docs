b31ca7354087950cf2e4a2761752d31b60f6e729
sidiDev <sididev3@gmail.com>
2020-11-04 15:40:58 +0000

    class changed to className

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -39,35 +39,35 @@ const Navbar = () => {
         </div>
 
 
-        <div class={`absolute top-0 inset-x-0 py-3 bg-white shadow-md transition transform origin-top-right md:hidden ${isOpen ? '' : 'hidden'}`}>
-            <div class="rounded-lg">
-            <div class="rounded-lg overflow-hidden" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
-                <div class="px-4 flex items-center justify-between">
+        <div className={`absolute top-0 inset-x-0 py-3 bg-white shadow-md transition transform origin-top-right md:hidden ${isOpen ? '' : 'hidden'}`}>
+            <div className="rounded-lg">
+            <div className="rounded-lg overflow-hidden" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
+                <div className="px-4 flex items-center justify-between">
                 <div>
                     <Link to="#" aria-label="Home" className="flex items-center text-xl">
                         <FaCode className="text-indigo-500 text-2xl"/>
                         <span className="ml-2">Object-Mds</span>
                     </Link>
                 </div>
-                <div class="-mr-2">
-                    <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Close menu"
+                <div className="-mr-2">
+                    <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Close menu"
                         onClick={() => setIsOpen(false)}
                     >
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                     </button>
                 </div>
                 </div>
-                <div class="px-2 pt-2 pb-3">
-                    <Link to="/docs/find" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">Find</Link>
-                    <Link to="/docs/Sort" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Sort</Link>
-                    <Link to="/docs/Limit" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Limit</Link>
-                    <Link to="/docs/Reverse" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Reverse</Link>
-                    <Link to="/docs/skip" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Skip</Link>
+                <div className="px-2 pt-2 pb-3">
+                    <Link to="/docs/find" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">Find</Link>
+                    <Link to="/docs/Sort" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Sort</Link>
+                    <Link to="/docs/Limit" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Limit</Link>
+                    <Link to="/docs/Reverse" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Reverse</Link>
+                    <Link to="/docs/skip" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Skip</Link>
                 </div>
                 <div>
-                <a href="https://github.com/sidiDev/Object-Mds" target="_blank" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-100 hover:bg-gray-200 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out" role="menuitem">
+                <a href="https://github.com/sidiDev/Object-Mds" target="_blank" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-100 hover:bg-gray-200 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out" role="menuitem">
                     Github
                 </a>
                 </div>
